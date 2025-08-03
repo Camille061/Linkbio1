17eb32c0616a8d96b0ce02bca02495924e690e28
Camille061 <slideshort72@gmail.com>
2025-08-03 12:05:06 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let firstname = "earth"
+let lastname = "eiei"
+
+let greet = `${firstname} ${lastname}`
+
+console.log(greet) 
+
+let age = 10;
+
+if (age >= 20) {
+    console.log("eiei")
+} else {
+    console.log("pp")
+}
